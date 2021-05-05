171bd62cb472cb3273d58cec50ba65302d71d168
Özgün Yildiz <ozgun.yildiz@hotmail.de>
2021-05-05 12:16:10 +0200

    WebKit Project

---
+++ Project4/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Özgün Yildiz on 05.05.21.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    
+}
+
